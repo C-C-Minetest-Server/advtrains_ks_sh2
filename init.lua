5359897394843522aa7ca4b09bafda27b03b857b
1F616EMO <root@1f616emo.xyz>
2025-04-12 22:24:38 +0800

    advtra___

--- init.lua
+++ init.lua
@@ -6,15 +6,26 @@
 local S = minetest.get_translator("advtrains_ks_sh2")
 local rotation_sbox = { -1 / 4, -1 / 2, -1 / 4, 1 / 4, -7 / 16, 1 / 4 }
 local rtn_sh2_aspect = function()
-    return advtrains.interlocking.DANGER
+    return {
+        main = 0,
+        shunt = false,
+        proceed_as_main = true,
+        dst = false,
+        info = {}
+    }
 end
 
-advtrains.trackplacer.register_tracktype("advtrains_ks_sh2:sh2")
+local function on_rightclick(pos, _, player)
+    local pname = player:get_player_name()
+    if pname == "" then return end
+    return advtrains.interlocking.show_ip_form(pos, pname)
+end
+
 for _, rtab in ipairs({
-    { rot = "0",  sbox = { -1 / 8, -1 / 2, -1 / 2, 1 / 8, 1 / 2, -1 / 4 },  ici = true },
-    { rot = "30", sbox = { -3 / 8, -1 / 2, -1 / 2, -1 / 8, 1 / 2, -1 / 4 }, },
-    { rot = "45", sbox = { -1 / 2, -1 / 2, -1 / 2, -1 / 4, 1 / 2, -1 / 4 }, },
-    { rot = "60", sbox = { -1 / 2, -1 / 2, -3 / 8, -1 / 4, 1 / 2, -1 / 8 }, },
+    { rot = "0",  sbox = { -1 / 8, -1 / 2, -1 / 2, 1 / 8, 1 / 2, -1 / 4 },  ici = true, nextrot = "30" },
+    { rot = "30", sbox = { -3 / 8, -1 / 2, -1 / 2, -1 / 8, 1 / 2, -1 / 4 }, nextrot = "45" },
+    { rot = "45", sbox = { -1 / 2, -1 / 2, -1 / 2, -1 / 4, 1 / 2, -1 / 4 }, nextrot = "60"},
+    { rot = "60", sbox = { -1 / 2, -1 / 2, -3 / 8, -1 / 4, 1 / 2, -1 / 8 }, nextrot = "0"},
 }) do
     minetest.register_node("advtrains_ks_sh2:sh2_sh2_" .. rtab.rot, {
         description = S("Ks Sh2 Signal"),
@@ -37,7 +48,7 @@ for _, rtab in ipairs({
         },
         groups = {
             cracky = 2,
-            advtrains_signal = 2,
+            advtrains_signal = 1,
             not_blocking_trains = 1,
             save_in_at_nodedb = 1,
             not_in_creative_inventory = rtab.ici and 0 or 1,
@@ -45,14 +56,21 @@ for _, rtab in ipairs({
         drop = "advtrains_ks_sh2:sh2_sh2_0",
         inventory_image = "advtrains_ks_sh2_sh2.png",
         advtrains = {
-            get_aspect = rtn_sh2_aspect
+            main_aspects = {
+                {
+                    name = "proceed",
+                    description = "Proceed",
+                }
+            },
+            get_aspect_info = rtn_sh2_aspect,
+            route_role = "end",
+            trackworker_next_rot = "advtrains_ks_sh2:sh2_sh2_"..rtab.nextrot,
+            trackworker_rot_incr_param2 = (rtab.rot=="60")
         },
-        on_rightclick = advtrains.interlocking.signal_rc_handler,
-        can_dig = advtrains.interlocking.signal_can_dig,
-        after_dig_node = advtrains.interlocking.signal_after_dig,
+        on_rightclick = on_rightclick,
+        can_dig = advtrains.interlocking.signal.can_dig,
+        after_dig_node = advtrains.interlocking.signal.after_dig,
     })
-
-    advtrains.trackplacer.add_worked("advtrains_ks_sh2:sh2", "sh2", "_" .. rtab.rot)
 end
 
 local sign_material = "default:sign_wall_steel" --fallback
